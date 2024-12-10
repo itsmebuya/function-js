0ca6e59b00c0432beb2ae12006986345d0a32e34
24LP7490 24LP7490 <itsmebuya@gmail.com>
2024-12-10 18:07:18 +0800

    bodlogo

---
+++ index.js
@@ -0,0 +1,87 @@
+//1 
+
+// function fizzBuzz (a){
+//     if(a%5 == 0 && a%3 ==0) {
+//         return "FizzBuzz"
+//     } else if(a%3 == 0) {
+//         return "Fizz"
+//     }else if(a%5==0){
+//         return "Buzz"
+//     }
+//     else{ 
+//         return a
+//     }
+// }
+// console.log(fizzBuzz(11))
+
+//2
+
+// function factorial(a) {
+//     if (a === 0 || a === 1) return 1;
+    // return a * factorial(a - 1);
+// }
+// console.log(factorial(6))
+
+//3
+// function oddEven(n){
+//     if(n%2==0) return "Even"
+//     return "Odd"
+// }
+// console.log(oddEven(1))
+
+//4
+// function addUp(n) {
+//     if (n === 0 || n === 1) return 1;
+//     return n + addUp(n-1)
+// }
+// console.log(addUp(7))
+
+//5
+// function isEmpty(a){
+//     if(a==="") return true
+//     return false
+// }
+
+// console.log(isEmpty(""))
+
+//6
+// function isEqual(a, b){
+//     if(a.length == b.length) return true
+//     return false
+// }
+// console.log(isEqual("asd", "qe"))
+
+//7
+// function divideBy(n, m) {
+//     if(n%m==0) return true
+//     return false
+// }
+// console.log(divideBy(10,6))
+
+//8
+// function moodToday(mood = "neuter"){
+//     return `Today, i am feeling ${mood}`
+// }
+// console.log(moodToday("Sad"));
+
+//9
+// function match(a,b){
+//     if(a.toUpperCase()===b.toUpperCase()) return true
+//     return false
+// }
+// console.log(match("asd", "ASD"))
+
+//10
+// function getCase(a){
+//     if(a===a.toUpperCase()) return "upper"
+//     return "lower"
+// }
+// console.log(getCase("ASD"))
+
+//11
+// function swapName(a,b) {
+//     return `${b} ${a}`
+// }
+// console.log(swapName("G", "B"))
+
+//12
