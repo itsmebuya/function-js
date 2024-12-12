edfd586757f05a4630b45f9883bddbf93039d0e2
24LP7490 24LP7490 <itsmebuya@gmail.com>
2024-12-12 18:11:08 +0800

    array and obj

---
+++ array.js
@@ -0,0 +1,75 @@
+//1
+// let arr = [1, 2, 3, 4]
+
+// for(let i =0; i<arr.length; i++){
+//     arr[i]= arr[i]*2
+// }
+// console.log(arr)
+
+//2
+// let arr = [1,2,3]
+// for(let i = 0; i<arr.length; i++){
+//     arr[i]= arr[i].toString()
+// }
+// console.log(arr)
+
+//3
+// let arr = ["john", 'jacob', 'qwerYy']
+// let name = []
+// for(let i=0; i<arr.length; i++) {
+//     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1).toLowerCase()
+// }
+// console.log(arr)
+
+//4
+// let obj =
+//     [{
+//         name: "Angelina Jolie",
+//         age: 80,
+//     },
+//     {
+//         name: "Eric Jones",
+//         age: 2,
+//     },
+//     {
+//         name: "Paris Hilton",
+//         age: 5,
+//     }]
+// let name = []
+// for(let i =0; i<obj.length; i++){
+//     name.push(obj[i].name)
+//     // obj[i].name.push
+// }
+// console.log(name)
+
+//5
+// let obj = [
+//     {
+//         name: "Angelina Jolie",
+//         age: 80
+//     },
+//     {
+//         name: "Eric Jones",
+//         age: 2
+//     },
+//     {
+//         name: "Paris Hilton",
+//         age: 5
+//     },
+//     {
+//         name: "Kayne West",
+//         age: 16
+//     },
+// ]
+// let arr = []
+// for(let i =0; i<obj.length; i++){
+//     if(obj[i].age>21){
+//         arr.push(obj[i].name + ' can go to matrix')
+//     } else {
+//         arr.push(obj[i].name + ' cant go to matrix')
+//     }
+    
+// }
+// console.log(arr)
+
+//6
